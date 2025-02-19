9d3b2b1d1cebe56a19065740067f116e06912302
jjs9000 <farriszamrie15@gmail.com>
2025-02-19 22:07:16 +0800

    replaced title logo and google font to fira code monospace

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -25,7 +25,7 @@ export const LoadingScreen = ({ onComplete }) => {
 
     return (
         <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
-            <div className="mb-4 text-4xl font-mono font-bold">
+            <div className="mb-4 text-4xl font-mono">
                 {text} <span className="animate-blink ml-1"> | </span>
             </div>
 
