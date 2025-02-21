9e955f5026d93526ad5615787aaaa3130fa7e02c
jjs9000 <farriszamrie15@gmail.com>
2025-02-22 00:08:36 +0800

    added projects section with styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Navbar } from './components/Navbar';
 import { MobileMenu } from './components/MobileMenu';
 import { Home } from './components/sections/Home';
 import { About } from './components/sections/About';
+import { Project } from './components/sections/Project';
 import "./index.css"
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
           <Home />
           <About />
+          <Project />
         </div>
     </>
   );
